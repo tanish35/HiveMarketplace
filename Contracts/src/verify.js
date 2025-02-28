bce95f3489f70293490dce7c2025dfa731227432
tanish35 <tanishmajumdar2912@gmail.com>
2025-02-28 21:41:56 +0530

    Minor change

--- Contracts/src/verify.js
+++ Contracts/src/verify.js
@@ -1,16 +1,18 @@
-const { PrivateKey } = require('@hiveio/dhive');
+const { PrivateKey } = require("@hiveio/dhive");
+const dotenv = require("dotenv");
+dotenv.config({ path: "../.env" });
 
 function isValidPrivateKey(privateKeyString) {
   try {
     const privateKey = PrivateKey.fromString(privateKeyString);
-    console.log('Private key is valid.');
+    console.log("Private key is valid.");
     return true;
   } catch (error) {
-    console.error('Invalid private key:', error.message);
+    console.error("Invalid private key:", error.message);
     return false;
   }
 }
 
 // Example usage
-const testKey = '5KC3W4qRSqH5W5z63wSqfmiCSBws4D6PHbkmRu3gyqjSeSfDZ8L';
+const testKey = process.env.PRIVATE_KEY;
 isValidPrivateKey(testKey);
